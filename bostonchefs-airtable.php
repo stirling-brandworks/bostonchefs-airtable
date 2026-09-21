f44fbdf55b961871661abe4ec2db3568ae273188
Sarah Silvia <sarah@stirlingbrandworks.com>
2026-09-21 13:52:04 -0400

    Delete bostonchefs-airtable.php

--- bostonchefs-airtable.php
+++
@@ -1,7 +0,0 @@
-<?php 
-/**
- * Plugin Name: BostonChefs Airtable Integration
- * Description: Syncs BostonChefs Airtable Rundown data into WordPress Restaurant Rundowns through a secure custom REST API.
- * Version: 1.0.0
- * Author: Stirling Brandworks
- */
